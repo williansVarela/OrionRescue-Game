1322bcf80a8f0d5440646cc35647e906850aa7fb
Willians Varela <willians.varela@gmail.com>
2018-09-03 19:36:30 -0300

    set max value for spaceship velocity

--- state1.js
+++ state1.js
@@ -58,6 +58,7 @@ orionRescue.state1.prototype = {
     spaceship.anchor.setTo(0.5, 0.5);
     game.physics.enable(spaceship, Phaser.Physics.ARCADE);
     spaceship.body.drag.x = 5500;
+    spaceship.body.maxVelocity.x = 2000; //Set max velocity for spaceship
     spaceship.body.collideWorldBounds = true;
 
     rocks = game.add.group();
